fc1e85d8df1eb5f7a45f198c42deaafdd8af3a1b
Alexei_1966 <Alexei_1966@outlook.com>
2024-06-14 21:34:54 +0300

    Создание основных классов

--- 1/test/Program.cs
+++ 1/test/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-using CefSharp.Wpf;
+//using CefSharp.Wpf;
 using System;
 using System.Drawing;
 using System.Text;
